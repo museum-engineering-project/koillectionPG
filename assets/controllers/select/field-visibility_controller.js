8b230b5fdaf5eec1082686cb15e77a0f4732659c
Lenetis <70149832+Lenetis@users.noreply.github.com>
2023-10-09 12:23:01 +0200

    Add datum and template fields visibility (#7)
    * Setting datum visibility prototype
    
    When adding an item, you can prefix the datum names with underscores to make them private.
    When editing an item, you can type "private", "public" or "internal" in the datums input boxes to set their visibility.
    
    (Currently the first method doesn't work when editing items, and the second one doesn't work when adding them. This will be fixed in a future commit)
    
    * Setting datum visibility
    
    * Set visibility in template fields
    
    * Added visibility selector to datum twig
    
    * Added visibility to FieldFactory
    
    * Database migration for adding visibility to template fields
    
    * Changed "page"->"field" in fields' visibility descriptions
    
    * templates: Fix styles and applying visibility for template fields
    
    ---------
    
    Co-authored-by: Bartosz Leśniewski <blesniewski@outlook.com>

---
+++ assets/controllers/select/field-visibility_controller.js
@@ -0,0 +1,52 @@
+import { Controller } from '@hotwired/stimulus';
+import Translator from "bazinga-translator";
+import { TsSelect2 } from "../../node_modules/ts-select2/dist/core";
+
+/* stimulusFetch: 'lazy' */
+export default class extends Controller {
+    connect() {
+        let self = this;
+
+        new TsSelect2(this.element, {
+            templateSelection: function (visibility) {
+                if (!visibility.id) {
+                    return self.htmlToElement('<span class="select-placeholder">' + Translator.trans('select2.none') + '</span>');
+                }
+
+                return self.htmlToElement(
+                    '<div>' + self.getIcon(visibility.element.value) + '<span>' + visibility.text + '</span></div>'
+                );
+            },
+
+            templateResult: function (visibility) {
+                if (!visibility.id) {
+                    return '';
+                }
+
+                return self.htmlToElement(
+                    '<div>' + self.getIcon(visibility.element.value) + '<span>' + visibility.text + '</span><span class="select-tip">' + Translator.trans('global.visibilities.' + visibility.id + '.field-description') + '</span></div>'
+                );
+            },
+        })
+    }
+
+    htmlToElement(html) {
+        let template = document.createElement('template');
+        html = html.trim();
+        template.innerHTML = html;
+        return template.content.firstChild;
+    }
+
+    getIcon(value) {
+        switch (value) {
+            case 'public':
+                return '<i class="select-icon fa fa-globe fa-fw"></i><i class="select-icon fa fa-unlock-alt fa-fw fa-secondary"></i>';
+            case 'private':
+                return '<i class="select-icon fa fa-lock fa-fw"></i>';
+            case 'internal':
+                return '<i class="select-icon fa fa-user fa-fw"></i><i class="select-icon fa fa-unlock-alt fa-fw fa-secondary"></i>';
+        }
+
+        return '';
+    }
+}
